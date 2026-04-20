061c16c923fc29aebcb567690ee5f3093d218c87
Rohan <rohankademani@outlook.com>
2026-04-20 20:37:35 +0530

    Fixed GRPC Service creation

--- main.tf
+++ main.tf
@@ -27,54 +27,11 @@ module "monitoring" {
   monitoring_namespace = var.monitoring_namespaces["global"]
 
 thanos_stores = [
-  "${kubernetes_service.thanos_a.metadata[0].name}.monitoring-cluster-a.svc.cluster.local:10901",
-  "${kubernetes_service.thanos_b.metadata[0].name}.monitoring-cluster-b.svc.cluster.local:10901"
+  "prometheus-cluster-a-thanos-discovery.monitoring-cluster-a.svc.cluster.local:10901",
+  "prometheus-cluster-b-thanos-discovery.monitoring-cluster-b.svc.cluster.local:10901"
 ]
 
   grafana_admin_password = var.grafana_admin_password
 
 }
 
-resource "kubernetes_service" "thanos_a" {
-  metadata {
-    name      = "prometheus-cluster-a-thanos"
-    namespace = var.monitoring_namespaces["cluster_a"]
-  }
-
-  spec {
-    selector = {
-      "app.kubernetes.io/name"       = "prometheus"
-      "operator.prometheus.io/name"  = "prometheus-cluster-a-prometheus"
-    }
-
-    port {
-      name        = "grpc"
-      port        = 10901
-      target_port = 10901
-    }
-
-    type = "ClusterIP"
-  }
-}
-
-resource "kubernetes_service" "thanos_b" {
-  metadata {
-    name      = "prometheus-cluster-b-thanos"
-    namespace = var.monitoring_namespaces["cluster_b"]
-  }
-
-  spec {
-    selector = {
-      "app.kubernetes.io/name"       = "prometheus"
-      "operator.prometheus.io/name"  = "prometheus-cluster-b-prometheus"
-    }
-
-    port {
-      name        = "grpc"
-      port        = 10901
-      target_port = 10901
-    }
-
-    type = "ClusterIP"
-  }
-}
